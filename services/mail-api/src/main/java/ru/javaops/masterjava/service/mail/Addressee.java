e460e390e719ee04e22236d30efad4317d361712
Vladimir <efimov_94@mail.ru>
2019-08-16 11:21:38 +0300

    8 6 fix wsdl and schema

--- services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/Addressee.java
+++ services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/Addressee.java
@@ -7,7 +7,7 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlValue;
 
-@Data
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "email")
